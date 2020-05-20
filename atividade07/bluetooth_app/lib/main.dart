552e2cb2d3e8e18a0a990cb1386eb17c16cefa0e
gabrielcastellani <gabrielcastellanioliveira@gmail.com>
2020-05-19 23:18:11 -0300

    bluetooth app

--- atividade07/bluetooth_app/lib/main.dart
+++ atividade07/bluetooth_app/lib/main.dart
@@ -1,60 +1,12 @@
-import 'package:bluetoothapp/DevicesPageWidget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
-void main() => runApp(BluetoothAppWidget());
+import './MainPage.dart';
 
-class BluetoothAppWidget extends StatefulWidget {
-  @override
-  _BluetoothAppWidgetState createState() => _BluetoothAppWidgetState();
-}
+void main() => runApp(new ExampleApplication());
 
-class _BluetoothAppWidgetState extends State<BluetoothAppWidget> {
-  int _selectedIndex = 1;
-
-  void _onSelectedItem(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  _getPageItemTapped() {
-    if (_selectedIndex == 0) {
-      return DevicesPageWidget();
-    }
-  }
-
+class ExampleApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bluetooth App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Bluetooth App'),
-          centerTitle: true,
-        ),
-        body: _getPageItemTapped(),
-        bottomNavigationBar: BottomNavigationBar(
-          items: <BottomNavigationBarItem> [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.devices),
-              title: Text('Devices'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text('Home'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.message),
-              title: Text('Message'),
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.blue,
-          onTap: _onSelectedItem,
-        ),
-      ),
-    );
+    return MaterialApp(home: MainPage());
   }
 }
